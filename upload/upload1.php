d4146bacbd514e90e3223f7b7d30d2d6694cc3cc
Shivam <shivamjaiswal2312@gmail.com>
2019-03-12 12:05:48 +0530

    Initial commit

---
+++ upload/upload1.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V11</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+<!--===============================================================================================-->
+</head>
+<body class="container-login100">
+			<div class="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
+
+				<span class="login100-form-title p-b-55">
+					Upload
+				</span>
+
+				  <div class="ways">
+				    <ul>
+				      <li class="active">
+				        submit
+				      </li>
+				      
+				    </ul>
+				  </div>
+					<br>
+				  <div class="sections">
+
+				    <section class="active">
+				      <input type="text" placeholder="Title" id="title"/>
+				      <select id="category">
+				        <option value="Male">Male</option>
+				        <option value="Female">Female</option>
+
+				      </select>
+
+				      <div class="select-option">
+				        <div class="head">Category</div>
+				        <div class="option"></div>
+				      </div>
+
+
+				      <div class="images">
+				        <div class="pic">
+				          add
+				        </div>
+				      </div>
+
+				    </section>
+
+				    <section>
+				      <input type="text" placeholder="Topic" id="topic"/>
+				      <textarea placeholder="something..." id="msg"></textarea>
+				    </section>
+
+						<div class="">
+							<div class="container-login100-form-btn p-t-25">
+
+								<button class="login100-form-btn">
+									Send
+								</button>
+								<br><br><br><br>
+							</div>
+					  </div>
+
+				  </div>
+				</div>
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
